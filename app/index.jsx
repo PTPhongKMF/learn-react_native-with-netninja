e9e6d15195937d40d5d9903632ef8e39f91f4ac1
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-05-27 02:29:28 +0700

    lesson_2: using view, text, image and nativewind css

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,22 @@
 import "../index.css"
-import { StyleSheet, Text, View } from 'react-native'
+import { Image, StyleSheet, Text, View } from 'react-native'
 
 export default function index() {
   return (
-    <View>
-      <Text>home sweet home</Text>
+    <View className="flex-1">
+      <View className="flex-1 items-center justify-center bg-red-100">
+        <Text className="font-bold text-3xl">home sweet home 1</Text>
+        <Text>learning</Text>
+        <Text>right now</Text>
+      </View>
+
+      <Image className="flex-1 p-4" source={{uri: "https://onlinepngtools.com/images/examples-onlinepngtools/boat-on-foggy-lake.png"}}/>
+
+      <View className="flex-1 items-center justify-center bg-blue-100">
+        <Text className="font-bold text-3xl">home sweet home 2</Text>
+        <Text>learning</Text>
+        <Text>right now</Text>
+      </View>
     </View>
   )
 }
