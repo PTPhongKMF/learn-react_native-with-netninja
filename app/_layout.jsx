6d1af07338225f4fdf2fe81ca6470b1cc09bf861
PTPhong-PPL <PTPhongSWB@gmail.com>
2025-05-28 00:40:00 +0700

    lesson_5: dark/light mode

--- app/_layout.jsx
+++ app/_layout.jsx
@@ -1,14 +1,24 @@
-import { View, Text } from 'react-native'
+import { View, Text, useColorScheme } from 'react-native'
 import { Slot, Stack } from 'expo-router'
+import { colors } from '../constants/colors';
+import { StatusBar } from 'expo-status-bar';
 
 export default function RootLayout() {
+  const colorScheme = useColorScheme();
+  const theme = colors[colorScheme] ?? colors.light;
+
   return (
-      <Stack screenOptions={{
-        headerStyle: { backgroundColor: "#ddd" },
-        headerTintColor: "#ff0000"
+    <>
+<StatusBar value="auto" />
+
+    <Stack screenOptions={{
+        headerStyle: { backgroundColor: theme.bg },
+        headerTintColor: theme.text
       }}>
         <Stack.Screen name="index" options={{ title: "Home" }}/>
         <Stack.Screen name="about" options={{ title: "About" }}/>
       </Stack>
+    </>
+      
   )
 }
---
+++ constants/colors.js
@@ -0,0 +1,11 @@
+export const colors = {
+  dark: {
+    text: "#d4d4d4",
+    bg: "#252231"
+  },
+
+  light: {
+    text: "#625f72",
+    bg: "#e0dfe8"
+  }
+}
